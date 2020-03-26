e05c828bdfe73db6c92c371a36038a62a5100b7b
Shubham Jain <shubhamjain@Shubhams-MacBook-Pro.local>
2020-03-26 13:54:37 -0400

    50. Pow(x,n)

---
+++ BinarySearch/Pow(x,n).py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        if not n:
+            return 1
+        
+        if n < 0:
+            return 1/(self.myPow(x,-n))
+        
+        if not n%2:
+            y = self.myPow(x,n/2)
+            return y*y
+        
+        else:
+            return x* self.myPow(x, n-1)
