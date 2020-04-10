d813404f6a04740cd8400088e64094c6b60c90cf
Shubham Sanjay Jain <itshubhamjain@gmail.com>
2020-04-10 18:16:23 -0400

    Create Closest Binary Search Tree Value.py
    270. Closest Binary Search Tree Value

---
+++ BinarySearch/Closest Binary Search Tree Value.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def closestValue(self, root: TreeNode, target: float) -> int:
+        closest = root.val
+        while root :
+            closest = min(root.val,closest, key = lambda x: abs(target-x))
+            root = root.left if target < root.val else root.right
+        return closest
