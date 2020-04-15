aab1dd18d3f0c2f2d2202b790d4abffd9f7ec385
Shubham Sanjay Jain <itshubhamjain@gmail.com>
2020-04-14 22:26:20 -0400

    1130. Minimum Cost Tree From Leaf Values

--- Stack/Minimum Cost Tree From Leaf Values.py
+++ Stack/Minimum Cost Tree From Leaf Values.py
@@ -6,3 +6,20 @@ class Solution:
             print(arr[i-1:i]+arr[i+1:i+2])
             res += min(arr[i-1:i]+arr[i+1:i+2])*arr.pop(i)
         return res
+
+
+
+
+
+class Solution:
+def mctFromLeafValues(self, arr: List[int]) -> int:
+    res = 0
+    stack = [float('inf')]
+    for val in arr:
+        while stack[-1] <= val:
+            mid = stack.pop()
+            res += mid * min(val,stack[-1])
+        stack.append(val)
+    while len(stack)>2:
+        res += stack.pop()*stack[-1]
+    return res
